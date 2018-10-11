3af3f4ddd3a73e6d70dbc5b6250f7b2509e28b13
Gabriel <ciulei.gabriel@gmail.com>
2018-10-11 17:41:49 +0200

    Added public key attributes

--- Assets/Scripts/spawner.cs
+++ Assets/Scripts/spawner.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class spawner : MonoBehaviour {
 
     public GameObject prefab;
+    public KeyCode generateContinuously = KeyCode.B;
+    public KeyCode generateOne = KeyCode.G;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.B) || Input.GetKeyDown(KeyCode.Space))
+		if (Input.GetKey(generateContinuously) || Input.GetKeyDown(generateOne))
         {
             generateBall();
         }

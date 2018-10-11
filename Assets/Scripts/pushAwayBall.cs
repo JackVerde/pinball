a41dd7f6ccbc80c974818350afd26170479a7155
Riccardo Giorato <giorat.96@gmail.com>
2018-10-11 17:43:12 +0200

    Aggiunto Cilindro spingi via pallina

---
+++ Assets/Scripts/pushAwayBall.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pushAwayBall : MonoBehaviour {
+
+	public void OnCollisionEnter (Collision pallina) {
+        if (pallina.collider.name.Contains ("pallina")) {
+            var force = pallina.transform.position - transform.position;
+            force.Normalize ();
+            pallina.gameObject.GetComponent<Rigidbody>().AddForce (force * Random.Range (800, 1500));
+        }
+    } //OnTriggerEnter
+}
